bea693cbd7feaf9b05d61f8ac161c642ff864751
gitirana <thaygitirana@gmail.com>
2024-01-12 14:10:12 -0300

    feat: add prettier and eslint configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <>
-      <div>
-        <Button>Click me</Button>
-      </div>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
-
